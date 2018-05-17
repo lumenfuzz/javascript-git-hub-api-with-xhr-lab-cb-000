db493b1a9a2b4cca4487674d6cfa89a84311091f
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-17 22:35:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  const repoList = repos
   document.getElementById("repositories").innerHTML = repoList
 }
 
